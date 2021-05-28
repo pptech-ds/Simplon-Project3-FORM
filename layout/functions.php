49f75f5ec0fd5dce3037226c0e02dd0c47cf1399
pras <pptech.ds@gmail.com>
2021-05-28 11:36:09 +0200

    started to implement php functions

--- layout/functions.php
+++ layout/functions.php
@@ -5,4 +5,13 @@ function testEcho(string $testParam) :string
     return 'return from function testEcho in functions.php'.$testParam;
 }
 
+function getScore(array $arrayScore) :int
+{
+    $score = 0;
+    foreach ($arrayScore as $key => $value) { 
+        $score += {$value};
+    }
+    return $score;
+}
+
 ?>
